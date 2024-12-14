42df8b418b7ebb748036bc49f237d3f82eea8d91
烟尘 <2742015729@qq.com>
2024-12-14 12:15:15 +0800

    upate

---
+++ src/components/button.vue
@@ -0,0 +1,21 @@
+<template>
+    <div>
+        <h1>HelloWorld!!!</h1>
+
+        <div>
+            <el-button :icon="Search" circle />
+            <el-button type="primary" :icon="Edit" circle />
+            <el-button type="success" :icon="Check" circle />
+            <el-button type="info" :icon="Message" circle />
+            <el-button type="warning" :icon="Star" circle />
+            <el-button type="danger" :icon="Delete" circle />
+        </div>
+    </div>
+</template>
+
+<script setup>
+
+
+</script>
+
+<style lang="less" scoped></style>
